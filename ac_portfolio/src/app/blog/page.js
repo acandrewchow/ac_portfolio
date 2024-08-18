930e854d409c118f15d729790af9f79af0e7ccee
Andrew Chow <ac.andrewchow@gmail.com>
2024-08-18 14:18:09 -0400

    Updates blog card size

--- ac_portfolio/src/app/blog/page.js
+++ ac_portfolio/src/app/blog/page.js
@@ -7,14 +7,14 @@ import Footer from "../components/Footer";
 import BlogPostCard from "../components/BlogPostCard";
 import Link from "next/link";
 import BackToTopButton from "../components/BackToTopButton";
-import { blogPostData } from "../data/blogPostData"; 
+import { blogPostData } from "../data/blogPostData";
 
 export default function BlogHome() {
   const [darkMode, setDarkMode] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const postsPerPage = 5; 
-  const totalPages = Math.ceil(blogPostData.length / postsPerPage); 
+  const postsPerPage = 5;
+  const totalPages = Math.ceil(blogPostData.length / postsPerPage);
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -60,45 +60,47 @@ export default function BlogHome() {
         <section className="min-h-screen">
           <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
 
-          <div className="pt-10 pb-5 px-6 max-w-xl mx-auto">
-            <h1
-              className={`text-3xl font-bold ${
-                darkMode ? "text-white" : "text-gray-900"
-              }`}
-            >
-              Blog
-            </h1>
-            <p
-              className={`mt-2 ${
-                darkMode ? "text-gray-300" : "text-gray-600"
-              }`}
-            >
-              Documenting my adventures in the world of software
-            </p>
+          <div className="max-w-3xl mx-auto px-6 py-10">
+            <div className="text-left mb-10">
+              <h1
+                className={`text-3xl font-bold ${
+                  darkMode ? "text-white" : "text-gray-900"
+                }`}
+              >
+                Blog
+              </h1>
+              <p
+                className={`mt-2 ${
+                  darkMode ? "text-gray-300" : "text-gray-600"
+                }`}
+              >
+                Documenting my adventures in the world of software
+              </p>
+            </div>
+
+            {/* Blog Post Cards */}
+            <div className="space-y-8">
+              {currentPosts.map((post, index) => (
+                <div key={index}>
+                  <Link
+                    href={post.link}
+                    target="_blank"
+                    className="hover:opacity-80"
+                  >
+                    <BlogPostCard
+                      title={post.title}
+                      date={post.date}
+                      description={post.description}
+                      tags={post.tags}
+                      darkMode={darkMode}
+                    />
+                  </Link>
+                </div>
+              ))}
+            </div>
           </div>
 
-          <div className="max-w-xl mx-auto space-y-8 px-6 py-10">
-            {currentPosts.map((post, index) => (
-              <div key={index}>
-                <Link
-                  href={post.link}
-                  target="_blank"
-                  className="hover:opacity-80"
-                >
-                  <BlogPostCard
-                    title={post.title}
-                    date={post.date}
-                    description={post.description}
-                    tags={post.tags}
-                    darkMode={darkMode}
-                  />
-                </Link>
-              </div>
-            ))}
-          </div>
-          <div className="flex justify-center mt-8">
-            {renderPageNumbers()}
-          </div>
+          <div className="flex justify-center mt-8">{renderPageNumbers()}</div>
         </section>
         <Footer darkMode={darkMode} />
         <BackToTopButton />
