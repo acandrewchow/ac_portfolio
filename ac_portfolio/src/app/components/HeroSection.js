c6faca2d1ecc259a21ebd794538acf3299041590
Andrew Chow <ac.andrewchow@gmail.com>
2024-12-30 21:16:41 -0500

    Removes bubble logic + adds resume/picture embeds to messages

--- ac_portfolio/src/app/components/HeroSection.js
+++ ac_portfolio/src/app/components/HeroSection.js
@@ -5,46 +5,29 @@ import { AiOutlineArrowUp } from "react-icons/ai";
 import ProfilePicture from "../../../public/profile_pic_banff.jpg";
 
 const HeroSection = () => {
-  const getPlatform = () => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    if (/Android/.test(userAgent)) {
-      return "Android";
-    } else if (
-      /iPad|iPhone|iPod/.test(userAgent) ||
-      navigator.platform === "MacIntel"
-    ) {
-      return "iOS";
-    } else if (/Win/.test(navigator.platform)) {
-      return "Windows";
-    } else {
-      return "Unknown";
-    }
-  };
-
-  const [platform, setPlatform] = useState("");
-
   const [messages, setMessages] = useState([
     {
-      text: "Hello, I'm Andrew, a 5th-year Software Engineering student at the University of Guelph, set to graduate in May 2025. I am currently looking for New Grad Opportunities for Spring 2025. Feel free to take a look around my website!",
+      text: "Hello, I'm Andrew, a 5th-year Software Engineering student at the University of Guelph, set to graduate in May 2025. I am currently looking for New Grad roles for Spring 2025",
+      image: "/profile_pic_banff.jpg",
+      altText: "Andrew's Profile Picture",
       isSender: false,
     },
     {
-      text: "You can visit my blog where I talk about my internship experiences",
+      text: "You can visit my blog where I talk about my co-op experiences -",
       isSender: false,
-      link: "https://andrewchow.ca/blog",
+      link: "andrewchow.ca/blog",
     },
     {
       text: "Otherwise, feel free to take a sneak peek at my gallery",
       isSender: false,
-      link: "https://andrewchow.ca/gallery",
+      link: "andrewchow.ca/gallery",
     },
     {
       text: "Can I ask you more questions?",
       isSender: true,
     },
     {
-      text: "Sure thing! Ask away :)",
+      text: "Sure thing, Ask me anything! 🙂",
       isSender: false,
     },
   ]);
@@ -55,9 +38,7 @@ const HeroSection = () => {
   const handleSendMessage = () => {
     if (newMessage.trim()) {
       setMessages([...messages, { text: newMessage, isSender: true }]);
-
       generateReply(newMessage);
-
       setNewMessage("");
     }
   };
@@ -67,7 +48,7 @@ const HeroSection = () => {
 
     if (userMessage.toLowerCase().includes("experience")) {
       reply =
-        "I have experience in developing scalable applications and working with multiple languages and frameworks, ranging from Elixir to React!";
+        "I have experience in developing scalable applications and working with multiple languages and frameworks, ranging from Elixir to React! In addition, I'm a teaching assistant for various computer science courses, including UI Design, Intro/Intermediate Programming and Web Development";
     } else if (userMessage.toLowerCase().includes("about")) {
       reply =
         "I'm a Software Engineering student at the University of Guelph, and I enjoy working on challenging technical problems.";
@@ -75,7 +56,7 @@ const HeroSection = () => {
       reply = (
         <span>
           You can find my GitHub profile{" "}
-          <a href="github.com/acandrewchow" className="text-blue-500">
+          <a href="https://github.com/acandrewchow" className="text-blue-400">
             here
           </a>{" "}
         </span>
@@ -91,7 +72,7 @@ const HeroSection = () => {
           <a
             href="https://www.linkedin.com/in/acandrewchow"
             target="_blank"
-            className="text-blue-500"
+            className="text-blue-400"
             rel="noopener noreferrer"
           >
             linkedin.com/in/acandrewchow
@@ -101,15 +82,26 @@ const HeroSection = () => {
     } else if (userMessage.toLowerCase().includes("resume")) {
       reply = (
         <span>
-          You can find my resume at{" "}
-          <a
-            href="https://andrewchow.ca/resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-          >
-            https://andrewchow.ca/resume.pdf
-          </a>
+          You can view my resume below:
+          <div className="mt-4">
+            <iframe
+              src="https://www.andrewchow.ca/resume.pdf"
+              width="100%"
+              height="425"
+              className="rounded-lg border border-gray-300"
+              title="Andrew's Resume"
+            ></iframe>
+          </div>
+          <div className="mt-2">
+            <a
+              href="https://www.andrewchow.ca/resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400"
+            >
+              open resume
+            </a>
+          </div>
         </span>
       );
     } else if (
@@ -131,17 +123,13 @@ const HeroSection = () => {
     }, 1000);
   };
 
-  // Scrolls to the bottom of the chat container when new messages are added
+  // Scroll to the bottom of the chat container when new messages are added
   useEffect(() => {
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop =
         chatContainerRef.current.scrollHeight;
     }
   }, [messages]);
-
-  useEffect(() => {
-    setPlatform(getPlatform());
-  }, []);
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -190,9 +178,7 @@ const HeroSection = () => {
                 <div
                   className={`max-w-xs p-3 rounded-3xl text-sm break-words ${
                     msg.isSender
-                      ? platform === "iOS" || platform === "Mac"
-                        ? "bg-blue-500 text-white ml-auto"
-                        : "bg-green-500 text-white ml-auto"
+                      ? "bg-blue-500 text-white ml-auto"
                       : "bg-zinc-700 text-white"
                   }`}
                 >
@@ -210,6 +196,16 @@ const HeroSection = () => {
                     </>
                   ) : (
                     msg.text
+                  )}
+
+                  {msg.image && (
+                    <Image
+                      src={msg.image}
+                      alt={msg.altText || "Profile Picture"}
+                      width={200}
+                      height={125}
+                      className="rounded-lg mt-2 flex flex-col items-center"
+                    />
                   )}
                 </div>
               </motion.div>
@@ -229,13 +225,7 @@ const HeroSection = () => {
             />
             <button
               onClick={handleSendMessage}
-              className={`absolute right-2 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full ${
-                platform === "iOS" || platform === "Mac"
-                  ? "bg-blue-500 hover:bg-blue-600"
-                  : platform === "Windows"
-                  ? "bg-green-500 hover:bg-green-600"
-                  : "bg-gray-500 hover:bg-gray-600"
-              }`}
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full bg-blue-500 hover:bg-blue-600"
             >
               <AiOutlineArrowUp size={16} />
             </button>
